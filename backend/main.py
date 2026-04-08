593163b29ba3e9e4be837a571e5d38f723f96293
urlagushka <nikolai.turkin.main@gmail.com>
2026-04-08 23:43:46 +0300

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
 import os
 
 
 app = FastAPI(title="AudioInsight Backend", version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 UPLOAD_DIR = "uploads"
